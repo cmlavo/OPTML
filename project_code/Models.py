1e6dead22a67ddd743d0822144fb61b85329bc94
cmlavo <christophe.michaud-lavoie@epfl.ch>
2025-06-10 19:26:27 +0200

    Add mediumconvnet

--- project_code/Models.py
+++ project_code/Models.py
@@ -44,8 +44,31 @@ class ConvNet(nn.Module):
         x = self.fc2(x)  # logits, no softmax
 
         return x
-    
+# Medium ConvNet for MNIST
+class MediumConvNet(nn.Module):
+    def _init_(self):
+        super(MediumConvNet, self)._init_()
 
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)  # 28x28
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)  # 14x14
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)  # 7x7
+
+        self.fc1 = nn.Linear(128 * 3 * 3, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # 28x28 -> 14x14
+        x = self.pool(F.relu(self.conv2(x)))  # 14x14 -> 7x7
+        x = self.pool(F.relu(self.conv3(x)))  # 7x7 -> 3x3
+
+        x = x.view(x.size(0), -1)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.fc2(x)
+
+        return x
 
 # Small ConvNet for MNIST
 class SmallConvNet(nn.Module):
